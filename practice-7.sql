8774bcace7a4e06f34e2f08aa88f7bafcf6eb3ae
Reed Schultz <reed.schultz@kellogg.northwestern.edu>
2025-01-16 23:41:25 +0000

    finished practice 7

--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,10 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
+.mode columns
+.headers on
 
+SELECT property_type, count(listing_id), MAX (date_reviewed)
+FROM listings
+INNER JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY property_type;
